03ba696ab24e8a90b05bdafc29a62677b54bdc41
ssingularity <1755405701@qq.com>
2019-05-20 14:25:00 +0800

    fix a bug

--- src/main/java/cn/pipipan/eisproject/brokergatewayddd/axonframework/listener/OrderBlotterListener.java
+++ src/main/java/cn/pipipan/eisproject/brokergatewayddd/axonframework/listener/OrderBlotterListener.java
@@ -5,7 +5,9 @@ import cn.pipipan.eisproject.brokergatewayddd.domain.OrderBlotterDTO;
 import cn.pipipan.eisproject.brokergatewayddd.repository.OrderBlottleDTORepository;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderBlotterListener {
     @Autowired
     OrderBlottleDTORepository orderBlottleDTORepository;
