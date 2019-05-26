f3ea091ab62e3cd147924bfc171eebed69cfc34e
ssingularity <1755405701@qq.com>
2019-05-27 07:42:46 +0800

    init orderblotter

--- src/main/java/cn/pipipan/eisproject/brokergatewayddd/axonframework/listener/OrderBlotterListener.java
+++ src/main/java/cn/pipipan/eisproject/brokergatewayddd/axonframework/listener/OrderBlotterListener.java
@@ -2,7 +2,7 @@ package cn.pipipan.eisproject.brokergatewayddd.axonframework.listener;
 
 import cn.pipipan.eisproject.brokergatewayddd.axonframework.event.IssueOrderBlotterEvent;
 import cn.pipipan.eisproject.brokergatewayddd.domain.OrderBlotterDTO;
-import cn.pipipan.eisproject.brokergatewayddd.repository.OrderBlottleDTORepository;
+import cn.pipipan.eisproject.brokergatewayddd.repository.OrderBlotterDTORepository;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderBlotterListener {
     @Autowired
-    OrderBlottleDTORepository orderBlottleDTORepository;
+    OrderBlotterDTORepository orderBlotterDTORepository;
 
     @EventHandler
     public void on(IssueOrderBlotterEvent issueOrderBlotterEvent){
-        orderBlottleDTORepository.save(OrderBlotterDTO.createOrderBlotter(issueOrderBlotterEvent));
+        orderBlotterDTORepository.save(OrderBlotterDTO.createOrderBlotter(issueOrderBlotterEvent));
     }
 }
